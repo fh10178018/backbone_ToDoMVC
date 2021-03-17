75531f26e81c68b86bb5a283cc0bf905fe7db483
Han Fang （方晗） <1206758827@qq.com>
2021-03-17 17:11:15 +0800

    :wrench: configuration webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
-const htmlWebpackPlugin = require('html-webpack-plugin');  
-module.exports = {
-  plugins: [  
-    new webpack.HotModuleReplacementPlugin()
-  ],
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+console.log(__dirname)
+module.exports = {
+  devServer: {
+    contentBase: './build',
+    host: 'localhost',
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  plugins: [
+    new htmlWebpackPlugin({   //创建一个在内存中生成html页面的插件
+      template: path.join(__dirname, 'public/index.html'),   //指定模板页面
+      //将来会根据此页面生成内存中的页面
+      filename: 'index.html'   //指定生成页面的名称，index.html浏览器才会默认直接打开
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  entry: path.resolve(__dirname, 'src/index.js'), // 设置入口运行文件
+  mode: 'development' // 设置mode
 }
