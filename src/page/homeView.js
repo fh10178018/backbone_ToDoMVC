627368d1424016c2cdca83c53a8f8233c55389cc
Han Fang （方晗） <1206758827@qq.com>
2021-03-18 19:13:06 +0800

    :new: join the footer view

--- src/page/homeView.js
+++ src/page/homeView.js
@@ -1,6 +1,7 @@
 var Backbone = require('backbone')
 var List = require('../component/List')
 var Header = require('../component/Header')
+var Footer = require('../component/Footer')
 
 
 var homeView = new (Backbone.View.extend({ // 主界面组建视图
@@ -9,13 +10,16 @@ var homeView = new (Backbone.View.extend({ // 主界面组建视图
   initialize: function () {
     this.header = new Header()
     this.list = new List()
+    this.footer = new Footer()
     this.render()
   },
   render: function () { // 绑定渲染函数
     this.$el.append(this.header.el) // 插入header元素
-    this.$el.append(this.list.el) // 插入header元素\
+    this.$el.append(this.list.el) // 插入header元素
+    this.$el.append(this.footer.el) // 插入footer
     this.header.render()
     this.list.render()
+    this.footer.render()
     return this
   }
 }))
