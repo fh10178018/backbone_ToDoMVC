7d460505d597f12178f3307408f70e0d49f749f1
Han Fang （方晗） <1206758827@qq.com>
2021-03-18 19:11:32 +0800

    :lipstick: modify the style

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -10,7 +10,7 @@ var Header = Backbone.View.extend({// header组件视图
     this.input = new Input()
   },
   render: function () { // 绑定渲染函数
-    this.$el.append('<h1 style="font-size: 40px;line-height: normal;color: coral;top:-100px">基于Backbone的TodoMVC</h1>')
+    this.$el.append('<h1 style="font-size: 40px;line-height: normal;color: coral;">基于Backbone的TodoMVC</h1>')
     this.$el.append(this.input.el)
     this.$el.append(this.keySpan.el)
     this.keySpan.render("↲") // 渲染一个回车键
