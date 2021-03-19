14e01b792078cf3acd661447f64d75558c9674b1
Han Fang （方晗） <1206758827@qq.com>
2021-03-19 17:54:58 +0800

    :new: When the interface is initialized, the data in the database is displayed

--- src/component/List/toDoList.js
+++ src/component/List/toDoList.js
@@ -1,8 +1,12 @@
+/**
+ * 实现toDoList列表视图
+ */
 var Backbone = require('backbone')
 var _ = require('underscore')
 var toDoListCollection = require('../../model/toDoListModel')
+var getTime = require('../../util/getTime')
 
-var deleteIDList = [];
+var deleteIDList = []; // 该队列，用于多删除
 
 var DeleteButton = Backbone.View.extend({ // 删除button
   tagName: 'button',
@@ -11,7 +15,7 @@ var DeleteButton = Backbone.View.extend({ // 删除button
     "click": "onClick"
   },
   onClick: function () { // 按钮点击时
-    toDoListCollection.remove(deleteIDList)
+    toDoListCollection.delete(deleteIDList)
   }
 })
 
@@ -24,14 +28,18 @@ var Checkbox = Backbone.View.extend({ // 删除button
   events: {
     "click": "onClick"
   },
+  initialize: function () {
+    if (this.model.get("status")) this.$el.prop("checked", true);
+    else this.$el.prop("checked", false);
+  },
   onClick: function (event) { // 按钮点击时
-    var id = this.$el.parent().attr("id") // 从父级获取id属性
-    var model = toDoListCollection.get(id)
-    if (event.target.checked) {
-      deleteIDList.push(model)
+    if (event.target.checked) { // 点击成功，存入删除队列中，否则移除队列
+      deleteIDList.push(this.model)
     } else {
-      deleteIDList.splice(deleteIDList.indexOf(model), 1)
+      deleteIDList.splice(deleteIDList.indexOf(this.model), 1)
     }
+    console.log(this.model)
+    this.model.save({ "status": event.target.checked }) // 记录状态到数据中,并同步修改localstorage
   }
 })
 
@@ -39,12 +47,14 @@ var ToDoItem = Backbone.View.extend({// 组件整合
   tagName: 'li',
   className: 'editing',
   initialize: function () {
-    this.button = new DeleteButton()
-    this.checkbox = new Checkbox()
-    this.listenTo(this.model, "change", this.render);
+    this.$el.attr("id", this.model.cid) // 添加id方便删除
+    // 给子视图传递model，方便数据操作
+    this.button = new DeleteButton({ model: this.model })
+    this.checkbox = new Checkbox({ model: this.model })
+    this.listenTo(this.model, "add", this.render); // 当add时，更新数据，但是不能是change，出于业务考虑
   },
   template: _.template(`
-    <label><%- name %></label>
+    <label><%- name %><small>创建时间：<%- createTime %></small></label>
   `),
   events: {
     "click": 'onClick'
@@ -57,10 +67,10 @@ var ToDoItem = Backbone.View.extend({// 组件整合
     }
   },
   render: function () {
-    var model = this.model.toJSON()
-    this.$el.attr('id', this.model.cid) // 添加id删除标签，用于删除
+    var item = this.model.toJSON()
+    item["createTime"] = getTime(item.createTime) // 将数据中的时间戳转为时间
     this.$el.append(this.checkbox.render().el) // 加入删除Button
-    this.$el.append(this.template(model)); // 将基于underscore渲染的模版渲染在当前视图标签里
+    this.$el.append(this.template(item)); // 将基于underscore渲染的模版渲染在当前视图标签里
     this.$el.append(this.button.render().el) // 加入删除Button
     return this
   }
@@ -81,6 +91,12 @@ var toDoList = (new (Backbone.View.extend({// 组件整合
     this.$el.find("#" + model.cid).remove()
   },
   render: function () {
+    toDoListCollection.models.forEach(item => { // 根据localstorage, 实现数据初始化
+      this.addItem(item)
+      if (item.status) { // 
+        deleteIDList.push(item)
+      }
+    });
     return this
   }
 }))).render()
