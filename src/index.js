d0434173b4bf69537fe96893ff320c364b78258b
Han Fang （方晗） <1206758827@qq.com>
2021-05-11 17:14:32 +0800

    :new: Storage new and old value modification monitor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var Backbone = require('backbone')
 var Router = require('./router/router.js');
 require('./index.css')
+require('./util/storageemitter')
 Backbone.history.start(); // Router必须引入过来和它在一起
